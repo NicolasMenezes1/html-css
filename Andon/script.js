a993a5d8dd8d08ed6b7cb64fe13444343adc2de6
NicolasMenezes1 <135354623+NicolasMenezes1@users.noreply.github.com>
2023-10-10 15:38:23 -0300

    ..

---
+++ Andon/script.js
@@ -0,0 +1,9 @@
+function showpopup(){
+    let element = document.getElementById('pop_up')
+    element.classList.add('show-popup' )
+}
+
+function hidepopup(){
+    let element = document.getElementById('pop_up')
+    element.classList.remove('show-popup' )
+}
